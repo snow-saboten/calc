0be16499428f1cb4f4a1e64097e78534dd2b04ba
snow-saboten <snow.saboten74@gmail.com>
2023-09-04 18:16:03 +0900

    足し算機能の追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [left, setLeft] = useState('');
+  const [right, setRight] = useState('');
+
+  
   return (
-    <div>
-      電卓
-    </div>
+    <>
+      <h1>電卓</h1>
+      <div>
+        <h4>足し算</h4>
+        <input type='number' onChange={(event) => setLeft(event.target.value)}></input>+<input type='number' onChange={(event) => setRight(event.target.value)}></input><br></br>
+        答え：<p>{Number(left)+Number(right)}</p>
+      </div>
+    </>
+    
   );
 }
 
